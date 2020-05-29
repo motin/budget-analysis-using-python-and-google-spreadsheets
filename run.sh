af9336444f4640618de12acd2b14e18953b815c0
Motin <motin@motin.eu>
2020-05-29 21:57:11 +0300

    First notebook draft

--- run.sh
+++ run.sh
@@ -5,7 +5,5 @@ if [ ! -d ./venv ]; then
 fi
 source venv/bin/activate
 
-echo "* Parsing transactions and uploading to Google Spreadsheets"
-papermill ./budget-analysis.ipynb ./budget-analysis.papermill-output.ipynb -p run_transactions_flow True
-
-jupyter-notebook ./budget-analysis.papermill-output.ipynb
+echo "* Parsing transactions and uploading to Google Spreadsheets (launching the Notebook only on error)"
+papermill ./budget-analysis.ipynb ./budget-analysis.papermill-output.ipynb || jupyter-notebook ./budget-analysis.papermill-output.ipynb
